52da24281a5f503928f297069d79ad8f709376d2
katrin <andreevak53@gmail.com>
2019-01-12 21:20:50 +0000

    saved

--- index.js
+++ index.js
@@ -1,18 +1,4 @@
-// //1 1. Создать две функции и дать им осмысленные названия:
-// - первая функция принимает массив и колбэк (одна для всех вызовов)
-// - вторая функция (колбэк) обрабатывает каждый элемент массива (для каждого вызова свой callback)
-
-// Первая функция возвращает строку “New value: ” и результат обработки:
-
-// firstFunc([‘my’, ‘name’, ‘is’, ‘Trinity’], handler1) → “New value: MyNameIsTrinity”
-// firstFunc([10, 20, 30], handler2) → “New value: 100, 200, 300,”
-// firstFunc([{age: 45, name: ‘Jhon’}, {age: 20, name: ‘Aaron’}], handler3) →
-// “New value: Jhon is 45, Aaron is 20,”
-// firstFunc([‘abc’, ‘123’], handler4) → “New value: cba, 321,” // строки инвертируются
-
-
-// Подсказка: secondFunc должна быть представлена функцией, которая принимает
-// один аргумент (каждый элемент массива) и возвращает результат его обработки
+// 1
 
 let stringArr = ['my', 'name', 'is', 'Trinity'];
 let numsArr = [10, 20, 30];
@@ -64,12 +50,7 @@ console.log(mainFunction(reverseArr, reverse));
 console.log(mainFunction(usersArr, userString));
 
 
-// 2. Написать аналог метода every. Создайте функцию every, 
-// она должна принимать первым аргументом массив чисел 
-// (обязательно проверьте что передан массив) вторым аргументом callback 
-// функция должна возвращать true или false в зависимости от результата 
-// вызова callback (проверить число больше 5). Callback  должен принимать 
-// один элемент массива, его индекс в массиве и весь массив. 
+// 2. 
 
 function every(arr, fn) {
    if (!Array.isArray(arr)) return false;
@@ -87,9 +68,6 @@ console.log(numberss);
 
 
 // //3 
-//На основе массива [1,2,3,5,8,9,10] сформировать новый массив,
-// каждый элемент которого будет хранить информацию о числе и его четности:
-// [{digit: 1, odd: true}, {digit: 2, odd: false}, {digit: 3, odd: true}...]
 
 let numbers = [1,2,3,5,8,9,10];
 
@@ -114,7 +92,7 @@ let res = numArray(numbers, odd);
 console.log(res);
 
 
-//4 /+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//4 
 let arr = [12, 4, 50, 1, 0, 18, 40];
 
 function equalZero(number) {
@@ -123,7 +101,7 @@ function equalZero(number) {
  console.log(arr.every(equalZero));
  
  
- //5 ++++++++++++++++++++++++++
+ //5 
 
  let arr2 = ['yes', 'hello', 'no', 'easycode', 'what'];
  
@@ -132,7 +110,7 @@ function equalZero(number) {
  }
  console.log(arr2.some(arrLength));
  
- //6 +++++++++++++++++++++++++++++++++++
+ //6 
 let arrObj =  [
  {char:"a",index:12},
  {char:"w",index:8},
@@ -163,7 +141,7 @@ console.log(result); //
 
 
 
-//7 ++++++++++++++++++++++------------------
+//7 
 
 
 let arr3 = [  [14, 45],  [1],  ['a', 'c', 'd'] ];
@@ -175,16 +153,7 @@ let arr3 = [  [14, 45],  [1],  ['a', 'c', 'd'] ];
 
 console.log(arr3);
 
-//8 Есть массив объектов:
-// [
-//     {cpu: 'intel', info: {cores:2, сache: 3}},
-//     {cpu: 'intel', info: {cores:4, сache: 4}},
-//     {cpu: 'amd', info: {cores:1, сache: 1}},
-//     {cpu: 'intel', info: {cores:3, сache: 2}},
-//     {cpu: 'amd', info: {cores:4, сache: 2}}
-// ]
-
-// Отсортировать их по возрастающему количеству ядер (cores).
+//8 
 
 let compArray = [
 {cpu: 'intel', info: {cores:2, сache: 3}},
